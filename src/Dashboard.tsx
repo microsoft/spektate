981eb3484563a337d4939751140ba944173426d4
Samiya Akhtar <samiyaakhtar7@gmail.com>
2019-07-17 18:34:59 -0700

    Allow p3 to be started in between

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -36,18 +36,18 @@ class Dashboard extends React.Component<{}, IDashboardState> {
     let counter = 0;
     this.state.deployments.forEach((deployment) => {
       rows.push(<tr key={counter}>
-                  <td><a href={deployment.srcToDockerBuild.sourceVersionURL}>{deployment.commitId}</a></td>
-                  <td><a href={deployment.srcToDockerBuild.URL}>{deployment.srcToDockerBuild.id}</a></td>
-                  <td>{this.getIcon(deployment.srcToDockerBuild.result)}</td>
-                  <td>{deployment.srcToDockerBuild.startTime.toLocaleString()}</td>
-                  <td>{deployment.srcToDockerBuild.sourceBranch.replace("refs/heads/", "")}</td>
+                  <td>{deployment.srcToDockerBuild ? <a href={deployment.srcToDockerBuild.sourceVersionURL}>{deployment.commitId}</a> : "-" }</td>
+                  <td>{deployment.srcToDockerBuild ? <a href={deployment.srcToDockerBuild.URL}>{deployment.srcToDockerBuild.id}</a> : "-"}</td>
+                  <td>{deployment.srcToDockerBuild ? this.getIcon(deployment.srcToDockerBuild.result) : "-"}</td>
+                  <td>{deployment.srcToDockerBuild ? deployment.srcToDockerBuild.startTime.toLocaleString() : "-"}</td>
+                  <td>{deployment.srcToDockerBuild ? deployment.srcToDockerBuild.sourceBranch.replace("refs/heads/", "") : "-"}</td>
                   <td>{deployment.imageTag}</td>
-                  <td>{deployment.dockerToHldRelease ? <a href={deployment.dockerToHldRelease.URL}>{deployment.dockerToHldRelease!.id}</a> : ""}</td>
-                  <td>{deployment.dockerToHldRelease ? this.getIcon(deployment.dockerToHldRelease!.status) : ""}</td>
-                  <td>{deployment.hldToManifestBuild ? <a href={deployment.hldToManifestBuild.sourceVersionURL}>{deployment.hldCommitId}</a> : ""}</td>
-                  <td>{deployment.hldToManifestBuild ? <a href={deployment.hldToManifestBuild.URL}>{deployment.hldToManifestBuild!.id}</a> : ""}</td>
-                  <td>{deployment.hldToManifestBuild ? this.getIcon(deployment.hldToManifestBuild!.result) : ""}</td>
-                  <td>{deployment.hldToManifestBuild ? deployment.hldToManifestBuild!.finishTime.toLocaleString() : ""}</td>
+                  <td>{deployment.dockerToHldRelease ? <a href={deployment.dockerToHldRelease.URL}>{deployment.dockerToHldRelease!.id}</a> : "-"}</td>
+                  <td>{deployment.dockerToHldRelease ? this.getIcon(deployment.dockerToHldRelease!.status) : "-"}</td>
+                  <td>{deployment.hldToManifestBuild ? <a href={deployment.hldToManifestBuild.sourceVersionURL}>{deployment.hldCommitId}</a> : "-"}</td>
+                  <td>{deployment.hldToManifestBuild ? <a href={deployment.hldToManifestBuild.URL}>{deployment.hldToManifestBuild!.id}</a> : "-"}</td>
+                  <td>{deployment.hldToManifestBuild ? this.getIcon(deployment.hldToManifestBuild!.result) : "-"}</td>
+                  <td>{deployment.hldToManifestBuild ? (Number.isNaN(deployment.hldToManifestBuild!.finishTime.valueOf()) ? "-" : deployment.hldToManifestBuild!.finishTime.toLocaleString()) : "-"}</td>
                   <td>{deployment.duration()} minutes</td>
                   <td>{deployment.status()}</td>
                 </tr>);
@@ -58,15 +58,15 @@ class Dashboard extends React.Component<{}, IDashboardState> {
             <tr>
               <th>Commit</th>
               <th>SRC to ACR</th>
-              <th>Status</th>
+              <th>Result</th>
               <th>Start Time</th>
               <th>Source Branch</th>
               <th>Image Version</th>
               <th>ACR to HLD</th>
-              <th>Status</th>
+              <th>Result</th>
               <th>Commit</th>
               <th>HLD to Manifest</th>
-              <th>Status</th>
+              <th>Result</th>
               <th>End Time</th>
               <th>Duration</th>
               <th>Status</th>
@@ -93,7 +93,7 @@ class Dashboard extends React.Component<{}, IDashboardState> {
     if(status === "succeeded") {
       return <Icon style={{color: "green"}} iconName="CompletedSolid" />;
     } else if (status === undefined || status === "inProgress") {
-      return <Icon style={{color: "#c80000"}} iconName="CirclePauseSolid" />;
+      return <Icon style={{color: "blue"}} iconName="CirclePauseSolid" />;
     }
     return <Icon style={{color: "#c80000"}} iconName="StatusErrorFull" />;
   }
