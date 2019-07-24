f7e7083c0e61e4129bbbde687adc9e214fd99537
Samiya Akhtar <samiyaakhtar7@gmail.com>
2019-07-24 15:41:52 -0700

    Supporting a third key value pair in scripts

--- pipeline-scripts/update_pipeline.py
+++ pipeline-scripts/update_pipeline.py
@@ -7,7 +7,7 @@ def generate_row_key():
     return str(uuid.uuid4()).split('-')[-1]
 
 # Performs a look up based on filter_name:filter_value for the pipeline to update its details
-def update_pipeline(account_name, account_key, table_name, partition_name, filter_name, filter_value, name1, value1, name2=None, value2=None):
+def update_pipeline(account_name, account_key, table_name, partition_name, filter_name, filter_value, name1, value1, name2=None, value2=None, name3=None, value3=None):
     table_service = TableService(account_name=account_name, account_key=account_key)
     entities = table_service.query_entities(table_name, filter=filter_name + " eq '"+ filter_value + "'")
 
@@ -24,6 +24,11 @@ def update_pipeline(account_name, account_key, table_name, partition_name, filte
                 add = True
             entity[name2] = value2
         
+        if name3 != None and value3 != None:
+            if name3 in entity and entity[name3] != value3:
+                add = True
+            entity[name3] = value3
+
         if add == False:
             table_service.update_entity(table_name, entity)
             print("Updating existing entry")
@@ -36,10 +41,10 @@ def update_pipeline(account_name, account_key, table_name, partition_name, filte
         break
 
     if count == 0:
-        add_pipeline(account_name, account_key, table_name, partition_name, filter_name, filter_value, name1, value1, name2, value2)
+        add_pipeline(account_name, account_key, table_name, partition_name, filter_name, filter_value, name1, value1, name2, value2, name3, value3)
     print("Done")
 
-def add_pipeline(account_name, account_key, table_name, partition_name, filter_name, filter_value, name1, value1, name2=None, value2=None):
+def add_pipeline(account_name, account_key, table_name, partition_name, filter_name, filter_value, name1, value1, name2=None, value2=None, name3=None, value3=None):
     print("Adding a new entry")
     new_entry = {}
     new_entry["RowKey"] = generate_row_key()
@@ -48,6 +53,8 @@ def add_pipeline(account_name, account_key, table_name, partition_name, filter_n
     new_entry[name1] = value1
     if name2 != None and value2 != None:
         new_entry[name2] = value2
+    if name3 != None and value3 != None:
+        new_entry[name3] = value3
     print(new_entry)
     table_service = TableService(account_name=account_name, account_key=account_key)
     table_service.insert_entity(table_name, new_entry)
@@ -65,5 +72,7 @@ if __name__ == "__main__":
         update_pipeline(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8])
     elif len(sys.argv) == 11:
         update_pipeline(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9], sys.argv[10])
+    elif len(sys.argv) == 13:
+        update_pipeline(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9], sys.argv[10], sys.argv[11], sys.argv[12])
     elif len(sys.argv) == 4:
         list_all_entities(sys.argv[1], sys.argv[2], sys.argv[3])
