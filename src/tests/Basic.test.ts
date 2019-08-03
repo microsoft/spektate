3b8eb89d8de2f62c6cf0ceac611bfce61f03ca42
Samiya Akhtar <samiyaakhtar7@gmail.com>
2019-08-03 10:10:41 -0400

    Improving deployment test

--- src/tests/Basic.test.ts
+++ src/tests/Basic.test.ts
@@ -32,13 +32,34 @@ describe('cluster-sync', () => {
     });
 });
 
-describe('deployments', () => {
-    it('should return some deployments', () => {
+describe('deployment', () => {
+    it('should return a correct deployment on filter', () => {
         const srcPipeline = new AzureDevOpsPipeline(config.AZURE_ORG, config.AZURE_PROJECT, config.SRC_PIPELINE_ID);
         const hldPipeline = new AzureDevOpsPipeline(config.AZURE_ORG, config.AZURE_PROJECT, config.DOCKER_PIPELINE_ID, true);
         const clusterPipeline = new AzureDevOpsPipeline(config.AZURE_ORG, config.AZURE_PROJECT, config.HLD_PIPELINE_ID);
 
-        Deployment.getDeployments(config.STORAGE_PARTITION_KEY, srcPipeline, hldPipeline, clusterPipeline, (deployments: Deployment[]) => {
+        Deployment.getDeploymentsBasedOnFilters(config.STORAGE_PARTITION_KEY, srcPipeline, hldPipeline, clusterPipeline, "Staging", undefined, undefined, undefined, (deployments: Deployment[]) => {
+            let found5432 = false;
+            deployments.forEach((deployment) => {
+                if (deployment.srcToDockerBuild && deployment.srcToDockerBuild.id.toString() === "5432") {
+                    found5432 = true;
+                    expect(deployment.srcToDockerBuild.sourceVersion.substring(0,7)).to.equal("e3d6504");
+                    expect(deployment.srcToDockerBuild.result).to.equal("succeeded");
+
+                    if (deployment.dockerToHldRelease && deployment.dockerToHldRelease.id.toString() === "137") {
+                        expect(deployment.dockerToHldRelease.imageVersion).to.equal("hello-bedrock-master-5432");
+                        expect(deployment.dockerToHldRelease.id.toString()).to.equal("137");
+                        expect(deployment.dockerToHldRelease.status).to.equal("succeeded");
+                    }
+
+                    if (deployment.hldToManifestBuild && deployment.hldToManifestBuild.id.toString() === "5433") {
+                        expect(deployment.manifestCommitId).to.equal("801b241");
+                        expect(deployment.hldToManifestBuild.id.toString()).to.equal("5433");
+                        expect(deployment.hldToManifestBuild.result).to.equal("succeeded");
+                    }
+                }
+            });
+            expect(found5432).to.equal(true);
             expect(deployments.length).to.greaterThan(0);
         });
     });
