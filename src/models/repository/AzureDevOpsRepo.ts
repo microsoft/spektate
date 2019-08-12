536342e29bfcfa341b15f0d19981f2d1e27f33d1
Samiya Akhtar <samiyaakhtar7@gmail.com>
2019-08-12 10:50:00 -0700

    Completing support for azure devops repos by pulling in cluster sync tag successfully

--- src/models/repository/AzureDevOpsRepo.ts
+++ src/models/repository/AzureDevOpsRepo.ts
@@ -3,7 +3,8 @@ import { Author } from './Author';
 import { Repository } from './Repository';
 
 const authorInfoURL = "https://dev.azure.com/{organization}/{project}/_apis/git/repositories/{repositoryId}/commits/{commitId}?api-version=4.1";
-const manifestSyncTagURL = "https://dev.azure.com/{organization}/{project}/_apis/git/repositories/{repositoryId}/refs?filter=tags&api-version=4.1";
+const manifestSyncTagsURL = "https://dev.azure.com/{organization}/{project}/_apis/git/repositories/{repositoryId}/refs?filter=tags&api-version=4.1";
+const manifestSyncTagURL = "https://dev.azure.com/{organization}/{project}/_apis/git/repositories/{repositoryId}/annotatedtags/{objectId}?api-version=4.1-preview.1";
 
 export class AzureDevOpsRepo extends Repository {
     public org: string;
@@ -21,20 +22,22 @@ export class AzureDevOpsRepo extends Repository {
     public getManifestSyncState(callback: (syncCommit: string) => void): Promise<void> {
         let tags;
         return new Promise((resolve, reject) => {
-            HttpHelper.httpGet(manifestSyncTagURL.replace("{organization}", this.org).replace("{project}", this.project).replace("{repositoryId}", this.repo), (data) => {
+            HttpHelper.httpGet(manifestSyncTagsURL.replace("{organization}", this.org).replace("{project}", this.project).replace("{repositoryId}", this.repo), (data) => {
                 tags = data.data.value;
                 if (tags != null && tags.length > 0) {
-                    tags.array.forEach((tag: any) => {
-                        if (tag.name === "refs/tags/flux-sync") {
-                            HttpHelper.httpGet(tag.url, (syncStatus) => {
+                    // tslint:disable-next-line: prefer-for-of
+                    for (let i = 0; i < tags.length; i++) {
+                        if (tags[i].name === "refs/tags/flux-sync") {
+                            const objectId = tags[i].objectId;
+                            HttpHelper.httpGet(manifestSyncTagURL.replace("{organization}", this.org).replace("{project}", this.project).replace("{repositoryId}", this.repo).replace("{objectId}", objectId), (syncStatus) => {
                                 resolve();
                                 if (syncStatus != null) {
-                                    this.manifestSync = syncStatus.data.object.sha.substring(0, 7);
+                                    this.manifestSync = syncStatus.data.taggedObject.objectId.substring(0, 7);
                                     callback(this.manifestSync);
                                 }
-                            });
+                            }, this.accessToken);
                         }
-                    });
+                    }
                     
                 }
             }, this.accessToken);
