46d6f25688ccb94e1eb0d34fb0fec47a8410e8e9
Samiya Akhtar <samiyaakhtar7@gmail.com>
2019-08-30 17:10:35 -0700

    Use latest versions of node

--- release-cli.sh
+++ release-cli.sh
@@ -2,6 +2,6 @@
 
 rm -rf spex-* 
 tsc -p . 
-pkg build/dist/cli/spex.js --out-path spex-linux --target node12-linux-x64
-pkg build/dist/cli/spex.js --out-path spex-macos --target node12-macos-x64
-pkg build/dist/cli/spex.js --out-path spex-win --target node12-win-x64
+pkg build/dist/cli/spex.js --out-path spex-linux --target latest-linux-x64
+pkg build/dist/cli/spex.js --out-path spex-macos --target latest-macos-x64
+pkg build/dist/cli/spex.js --out-path spex-win --target latest-win-x64
