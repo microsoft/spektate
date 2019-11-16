901126c95be7b60c33063c76f3d02bdfd5dd1d4a
Samiya Akhtar <samiyaakhtar7@gmail.com>
2019-11-16 01:24:52 -0500

    Added unit tests for manfiest sync in github

--- packages/spektate/src/repository/GitHub.test.ts
+++ packages/spektate/src/repository/GitHub.test.ts
@@ -3,31 +3,41 @@ import * as fs from "fs";
 import { HttpHelper } from "../HttpHelper";
 import { IAuthor } from "./Author";
 import { GitHub } from "./GitHub";
+import { ITag } from "./Tag";
 
-let currentRawResponse = {};
+let authorRawResponse = {};
+let syncTagRawResponse = {};
+let manifestSyncTagResponse = {};
 const mockDirectory = "src/repository/mocks/";
+const repo = new GitHub("username", "reponame", "some-token");
 
+beforeAll(() => {
+  authorRawResponse = JSON.parse(
+    fs.readFileSync(mockDirectory + "github-author-response.json", "utf-8")
+  );
+  syncTagRawResponse = JSON.parse(
+    fs.readFileSync(mockDirectory + "github-sync-response.json", "utf-8")
+  );
+  manifestSyncTagResponse = JSON.parse(
+    fs.readFileSync(
+      mockDirectory + "github-manifest-sync-tag-response.json",
+      "utf-8"
+    )
+  );
+});
 jest.spyOn(HttpHelper, "httpGet").mockImplementation(
   <T>(theUrl: string, accessToken?: string): Promise<AxiosResponse<T>> => {
-    return new Promise(resolve => {
-      const response: AxiosResponse<any> = {
-        config: {},
-        data: currentRawResponse,
-        headers: "",
-        status: 200,
-        statusText: ""
-      };
-      resolve(response);
-    });
+    if (theUrl.includes("commits")) {
+      return getAxiosResponseForObject(authorRawResponse);
+    } else if (theUrl.includes("flux-sync")) {
+      return getAxiosResponseForObject(syncTagRawResponse);
+    }
+    return getAxiosResponseForObject(manifestSyncTagResponse);
   }
 );
 
 describe("GitHub", () => {
   test("gets author correctly", () => {
-    currentRawResponse = JSON.parse(
-      fs.readFileSync(mockDirectory + "github-author-response.json", "utf-8")
-    );
-    const repo = new GitHub("username", "reponame", "some-token");
     repo.getAuthor("commit").then((author: IAuthor | undefined) => {
       expect(author).toBeDefined();
       expect(author!.name).toBe("Edaena Salinas");
@@ -36,3 +46,29 @@ describe("GitHub", () => {
     });
   });
 });
+
+describe("GitHub", () => {
+  test("gets manifest sync tag correctly", () => {
+    repo.getManifestSyncState().then((tag: ITag) => {
+      expect(tag.commit).toBe("096c952");
+      expect(tag.date.getTime()).toBe(
+        new Date("2019-10-31T18:14:51.000Z").getTime()
+      );
+      expect(tag.tagger).toBeDefined();
+      expect(tag.tagger).toBe("Weave Flux");
+    });
+  });
+});
+
+const getAxiosResponseForObject = <T>(obj: any): Promise<AxiosResponse<T>> => {
+  return new Promise(resolve => {
+    const response: AxiosResponse<any> = {
+      config: {},
+      data: obj,
+      headers: "",
+      status: 200,
+      statusText: ""
+    };
+    resolve(response);
+  });
+};
