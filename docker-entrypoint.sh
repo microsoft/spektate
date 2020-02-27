647f13e4fd0de835a663c66515d90d07959ad9e3
Samiya Akhtar <samiyaakhtar7@gmail.com>
2020-02-27 15:45:55 -0800

    Finally working

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env sh
 set -eu
-# mainstring={
 for word in $(env | grep '^REACT_APP_*')
 do
     key=${word%%=*}
     value=${word#*=}
     pair=$(echo "\"$key\":\"$value\"")
-    # echo "\"$key\":\"$value\""
-    # mainstring="$mainstring $pair,"
     export $key=$value
 done
 
-# ts-node index.js
 node server.js
