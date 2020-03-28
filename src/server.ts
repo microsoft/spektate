28acc8cc40a783c8da8422d1d02326f68c4e7335
Dennis Seah <dennis.seah@gmail.com>
2020-03-27 17:54:34 -0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import * as express from "express";
 import * as path from "path";
 import { get as clusterSyncGet } from "./backend/clustersync";
-import { fetch as fetchDeployment } from "./backend/lib/cache";
+import { cacheRefreshInterval } from "./backend/config";
+import {
+  fetch as fetchDeployment,
+  update as updateCache
+} from "./backend/lib/cache";
 
 const app = express();
 
@@ -25,7 +29,13 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
+(async () => {
+  await updateCache();
 
-console.log(`Listening on ${port}`);
+  setInterval(async () => {
+    await updateCache();
+  }, cacheRefreshInterval());
+  const port = process.env.PORT || 5000;
+  app.listen(port);
+  console.log(`Listening on ${port}`);
+})();
