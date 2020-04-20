f3c5d11c31583583bcd7ebfbafc461582693edd6
Samiya Akhtar <samiyaakhtar7@gmail.com>
2020-04-19 18:21:41 -0700

    testing docker build

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -26,7 +26,7 @@ app.get("/api/pr", (req: express.Request, res: express.Response) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
 const port = process.env.PORT || 5000;
